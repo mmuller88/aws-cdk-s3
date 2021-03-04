072dc6879fc31ae444755be75c797e98953fe2e0
Martin Mueller <damadden88@googlemail.com>
2021-03-04 07:42:44 +0100

    type the json into ts json

--- src/s3-stack.ts
+++ src/s3-stack.ts
@@ -1,8 +1,6 @@
 import * as iam from '@aws-cdk/aws-iam';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as core from '@aws-cdk/core';
-// import { AppSyncTransformer } from 'cdk-appsync-transformer';
-
 
 export interface S3StackProps extends core.StackProps {
   readonly Name: string;
